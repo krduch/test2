6b6dd025c2d69ed5c7b67a0e52b82f3e5ae47c9e
krduch <123241324+krduch@users.noreply.github.com>
2023-11-16 18:33:54 +0100

    pierwszy commit

---
+++ 4_convert.py
@@ -0,0 +1,14 @@
+value = input("wprowadz walute zl or eu: ")
+num1 = float(input("podaj kwote jaka chcesz zmienić: "))
+value_course = 4.5
+
+if  value == "zl":
+   result= num1  / value_course
+   print(f"Posiadasz tyle {num1} zlotych a to tyle {result} euro ")
+else:
+    result = num1*value_course
+    print(f"Posiadasz tyle {num1} euro a to tyle {result} złotych ")
+
+
+
+
